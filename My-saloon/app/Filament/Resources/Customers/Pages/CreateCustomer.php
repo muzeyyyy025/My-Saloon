9e0ba75e65fc723be605f593d66cc0b91382f566
MUZAMMIL IMAD Hussain <muxamil025@gmail.com>
2025-11-26 00:37:24 +0500

    sample job created

--- My-saloon/app/Filament/Resources/Customers/Pages/CreateCustomer.php
+++ My-saloon/app/Filament/Resources/Customers/Pages/CreateCustomer.php
@@ -4,8 +4,13 @@ namespace App\Filament\Resources\Customers\Pages;
 
 use App\Filament\Resources\Customers\CustomerResource;
 use Filament\Resources\Pages\CreateRecord;
-
+use App\Jobs\AppointmentRemainderJob;
 class CreateCustomer extends CreateRecord
 {
     protected static string $resource = CustomerResource::class;
+
+protected function afterCreate(): void
+{
+    AppointmentRemainderJob::dispatch($this->record->id);
+}
 }
